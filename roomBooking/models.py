bb74e9298c2d6d00911eecfd01041d65b0076932
Ashley-Wilson1 <ashley03wilson@gmail.com>
2025-02-18 19:24:16 +0000

    Changes to models output

--- roomBooking/models.py
+++ roomBooking/models.py
@@ -9,20 +9,20 @@ class User(AbstractUser):
     def __str__(self):
         return self.email  
 
-class Room(models.Model):
+class Room(models.Model):#maybe give int pk for custom rooms
     capacity = models.IntegerField()
 
     def __str__(self):
         return f"Room {self.id} (Capacity: {self.capacity})"
 
 class RoomBooking(models.Model):
-    name = models.CharField(max_length=100)
+    
     start_datetime = models.DateTimeField()
     end_datetime = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)  
     room = models.ForeignKey(Room, on_delete=models.CASCADE, null=True, blank=True)  # Allow nulls to avoid migration issues
 
     def __str__(self):
-        return f"{self.name} by {self.user.username}"
+        return f"{self.user.first_name} has booked Room {self.room.id} from {self.start_datetime} to {self.end_datetime}"
 
 
