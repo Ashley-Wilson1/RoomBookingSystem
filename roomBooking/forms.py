0b9d372ffef8b4f1b60e07d2e08e74b4ddb83e30
Ashley-Wilson1 <ashley03wilson@gmail.com>
2025-02-25 18:29:28 +0000

    added widget for datetime form

--- roomBooking/forms.py
+++ roomBooking/forms.py
@@ -8,7 +8,11 @@ class AvailabilityForm(forms.Form):  # Fix typo (was AvailabiltyForm)
         super().__init__(*args, **kwargs)
         self.fields['room_number'].choices = [(room.number, room.number) for room in Room.objects.all()]
 
-    start_datetime = forms.DateTimeField(required=True,input_formats=["%y-%m-%dT%H:%M",])
-    end_datetime = forms.DateTimeField(required=True,input_formats=["%y-%m-%dT%H:%M",])
+    start_datetime = forms.DateTimeField(required=True,widget=forms.DateTimeInput(attrs={
+            'type': 'datetime-local',
+        }))
+    end_datetime = forms.DateTimeField(required=True,widget=forms.DateTimeInput(attrs={
+            'type': 'datetime-local',
+        }))
 
     want_another_room = forms.BooleanField(required=False, initial=False, label="Allow automatic booking of another room if requested room is unavailable")
